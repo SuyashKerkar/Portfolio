baeebd6c89b59143233290d80ce8e9233ebc7840
SuyashKerkar <suyashkasdf@gmail.com>
2024-12-06 00:19:55 +0530

    resume and connect button responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,16 +33,17 @@ const Hero = () => {
         </span>
         Full-Stack Developer
       </h1>
-      <p className="mt-4 text-lg text-gray-300 ">
+      <p className="mt-4 text-lg text-gray-300">
         I specialize in building modern and responsive web applications.
       </p>
-      <div className="mt-8 space-x-4">
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
+      {/* Button Container */}
+      <div className="mt-8 flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 justify-center items-center">
+        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
           Connect With Me
         </button>
         <button
           onClick={handleDownload}
-          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
         >
           Resume
         </button>
